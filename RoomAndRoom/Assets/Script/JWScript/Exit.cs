d53887523c1077125dfbb5797f462fe6ee5a9126
HWANGJUNGWOO <ghkdwnddn@hanmail.net>
2017-11-24 21:06:51 +0900

    no message

---
+++ RoomAndRoom/Assets/Script/JWScript/Exit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+    public GameObject eixtscreen;
+    public void ClickPicture()
+    {
+        eixtscreen.SetActive(true);
+        StartCoroutine(Off(5.0f));
+
+    }
+    IEnumerator Off(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        eixtscreen.SetActive(false);
+    }
+}
