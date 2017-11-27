ce8347e8bf8f84ff7146925eca37fbdd574b7432
DESKTOP-3IHOFI9\박요한 <dygks910910@daum.net>
2017-11-28 00:36:26 +0900

    오브젝트 정리 및 게임매니저오브젝트 작성중.

---
+++ RoomAndRoom/Assets/YHAsset/MyScript/GameManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour {
+    [Tooltip("피킹 가능 오브젝트 배열")]
+    public GameObject[] pickObj;
+	// Use this for initialization
+	void Start () {
+        //피킹가능 배열 리스트로 넣기.
+        pickObj = GameObject.FindGameObjectsWithTag("PickObject");
+       
+        foreach(GameObject gameobj in pickObj)
+        {
+            print("a");
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- RoomAndRoom/Assets/YHAsset/MyScript/scaleToIcon.cs
+++ RoomAndRoom/Assets/YHAsset/MyScript/scaleToIcon.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 //아이콘형태로 축소시키고 원복시킴.
 public class scaleToIcon : MonoBehaviour {
+    Vector3 originScale;
+    [Tooltip("스케일할 각각 벡터의 값.")]
+    public Vector3 scaleFactor;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        originScale = transform.localScale;
+    }
+    public void ScaleToIcon()
+    {
+        transform.localScale = scaleFactor;
+    }
+    public void ScaleToOrigin()
+    {
+        transform.localScale = originScale;
+    }
 }
