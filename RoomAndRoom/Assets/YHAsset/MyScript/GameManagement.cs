92aa57f03e0dc709bcc93bb1086ae48061191e8b
DESKTOP-3IHOFI9\박요한 <dygks910910@daum.net>
2017-12-06 20:28:40 +0900

    첫번째 방 완료.디자인만 더하면 됨.

--- RoomAndRoom/Assets/YHAsset/MyScript/GameManagement.cs
+++ RoomAndRoom/Assets/YHAsset/MyScript/GameManagement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameManagement : MonoBehaviour
 {
     [Tooltip("선택된 오브젝트")]
+    public bool isPicked = false;
     public GameObject pickedObj;
     public GameObject interactionObj;
     void Start()
@@ -13,24 +14,25 @@ public class GameManagement : MonoBehaviour
     }
     public void unPickObj()
     {
-        if(pickedObj != null)
-        pickedObj.GetComponent<PickingControll>().UnbindingCamera();
-    }
-    public void CheckInteraction()
-    {
         if (pickedObj != null)
         {
             pickedObj.GetComponent<PickingControll>().UnbindingCamera();
+            pickedObj = null;
+        }
+    }
+    public void CheckInteraction()
+    {
+        if (pickedObj != null && interactionObj!=null)
+        {
+            pickedObj.GetComponent<PickingControll>().UnbindingCamera();
+            isPicked = false;
 
             if (interactionObj.name == "SM_Bed_01_S" && pickedObj.name == "Leaver")
             {
                 //침대와의 상호작용
                 interactionObj.GetComponent<LerpToA>().isStarting = true;
-
-                interactionObj = null;
-                print("침대와의 상호작용");
             }
-            if (interactionObj.name == "Door1" && pickedObj.name == "Key1")
+            else if (interactionObj.name == "Door1" && pickedObj.name == "Key1")
             {
                 interactionObj.GetComponent<RotationDegreeAtoB>().StartRotate();
             }
@@ -38,6 +40,8 @@ public class GameManagement : MonoBehaviour
             {
                 interactionObj = null;
             }
+            pickedObj = null;
+            interactionObj = null; 
         }
         //침대와 지렛대가 맞다면 침대를 들어올린다.
 
--- RoomAndRoom/Assets/YHAsset/MyScript/PickingControll.cs
+++ RoomAndRoom/Assets/YHAsset/MyScript/PickingControll.cs
@@ -36,6 +36,8 @@ public class PickingControll : MonoBehaviour {
     }
     public void BindingCamera()
     {
+        if (gameManagementScript.isPicked == false)
+        {
             MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
             Material material = meshRenderer.material;
             this.transform.parent = cam.transform;
@@ -65,40 +67,46 @@ public class PickingControll : MonoBehaviour {
             rigidBody.isKinematic = true;
             colider.enabled = false;
             isPicked = true;
-       
+
             GetComponent<scaleToIcon>().ScaleToIcon();
-        //피키드 오브젝트에 자기자신을 묶어줌.
-        gameManagementScript.pickedObj = transform.gameObject;
+            //피키드 오브젝트에 자기자신을 묶어줌.
+            gameManagementScript.pickedObj = transform.gameObject;
+            gameManagementScript.isPicked = true;
+        }
     }
     public void UnbindingCamera()
     {
-        this.transform.parent = dynamicObjects.transform;
-        MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
-        Material material = meshRenderer.material;
-        //this.transform.parent = cam.transform;
-        //카메라앞으로 이동.
-        //transform.localPosition = new Vector3(0, 0, 1.5f);
-        material.SetFloat("_Mode", 4f);
-        //0f - opacity
-        //1f - cutout
-        //2f - fade
-        //3f - transparent
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        //블랭드 함.
-        // material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        //블렌드 안함.
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        material.DisableKeyword("_ALPHATEST_ON");
-        material.EnableKeyword("_ALPHABLEND_ON");
-        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = 3000;
-        Color32 col = meshRenderer.material.GetColor("_Color");
-        col.a = 255;
-        meshRenderer.material.SetColor("_Color", col);
-        GetComponent<scaleToIcon>().ScaleToOrigin();
-        colider.enabled = true;
-        isPicked = false;
-        rigidBody.isKinematic = false;
+        if (gameManagementScript.isPicked)
+        {
+            this.transform.parent = dynamicObjects.transform;
+            MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+            Material material = meshRenderer.material;
+            //this.transform.parent = cam.transform;
+            //카메라앞으로 이동.
+            //transform.localPosition = new Vector3(0, 0, 1.5f);
+            material.SetFloat("_Mode", 4f);
+            //0f - opacity
+            //1f - cutout
+            //2f - fade
+            //3f - transparent
+            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            //블랭드 함.
+            // material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            //블렌드 안함.
+            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            material.DisableKeyword("_ALPHATEST_ON");
+            material.EnableKeyword("_ALPHABLEND_ON");
+            material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            material.renderQueue = 3000;
+            Color32 col = meshRenderer.material.GetColor("_Color");
+            col.a = 255;
+            meshRenderer.material.SetColor("_Color", col);
+            GetComponent<scaleToIcon>().ScaleToOrigin();
+            colider.enabled = true;
+            isPicked = false;
+            rigidBody.isKinematic = false;
+            gameManagementScript.isPicked = false;
+        }
     }
     public void InteractionObjSendingToGameMgr()
     {
