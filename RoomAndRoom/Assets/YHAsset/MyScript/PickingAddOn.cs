7b7e2785df25568967d7a70c81840051bc588a90
DESKTOP-BSVS6V6\YoHan <dygks910910@daum.net>
2017-12-10 20:09:57 +0900

    Add Ak-47 Gun parts

---
+++ RoomAndRoom/Assets/YHAsset/MyScript/PickingAddOn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class PickingAddOn : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        //scaleToIcon은 수동으로 추가.
+        //eventTrigger는 수동으로 추가.
+        GameObject parentGameObj;
+        parentGameObj = transform.parent.gameObject;
+        //피킹에 필요한 컴포넌트 추가.
+        parentGameObj.AddComponent<BoxCollider>();
+        parentGameObj.AddComponent<PickingControll>();
+        parentGameObj.AddComponent<Rigidbody>();
+        
+
+        parentGameObj.AddComponent<EventTrigger>();
+        EventTrigger trigger =  parentGameObj.GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((eventData) => { parentGameObj.GetComponent<PickingControll>().BindingCamera(); });
+        trigger.triggers.Add(entry);
+    }
+	
+}
