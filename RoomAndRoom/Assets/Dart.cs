f38d1c9e4ae1b5e4319bc3d3c304ae84d1619ab1
HWANGJUNGWOO <ghkdwnddn@hanmail.net>
2017-12-13 02:56:58 +0900

    BedRoom (옵션창, 락패드, 망치, 메모장, 노트북 단서)

---
+++ RoomAndRoom/Assets/Dart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour {
+    public GameObject GvrPointer;
+    public static Dart DartIns;
+    public GameObject DDart; 
+    public void Awake()
+    {
+        Dart.DartIns = this;
+    }
+    public void SizeUp()
+    {
+        transform.localScale = new Vector3(4, 4, 4);
+    }
+    public void SizeDown()
+    {
+        transform.localScale = new Vector3(2, 2, 2);
+    }
+    public void ClickBoard()
+    {
+        DDart.SetActive(false);
+        DartBoard.dartinstence.PickCheck = false;
+        
+    }
+    public void GetDart(GameObject obj)
+    {
+        DDart = obj;
+        if (DartBoard.dartinstence.PickCheck == false)
+        {
+            transform.parent = GvrPointer.transform;
+            Collider col = GetComponent<Collider>();
+            DartBoard.dartinstence.GetDart = true;
+            col.enabled = false;
+            DartBoard.dartinstence.PickCheck = true;
+        }
+    }
+}
--- RoomAndRoom/Assets/Script/JWScript/DartBoard.cs
+++ RoomAndRoom/Assets/Script/JWScript/DartBoard.cs
@@ -6,6 +6,13 @@ public class DartBoard : MonoBehaviour {
     public GameObject Light;
     GameObject Ppos;
     GameObject Dpos;
+    public static DartBoard dartinstence;
+    public bool PickCheck = false;
+    public bool GetDart = false;
+    void Awake()
+    {
+        DartBoard.dartinstence = this;
+    }
     void Start()
     {
         Ppos = GameObject.Find("Player");
@@ -21,5 +28,18 @@ public class DartBoard : MonoBehaviour {
     public void DartEixt()
     {
         Light.SetActive(false);
+    }
+    public void Update()
+    {
+        if (GetDart == true)
+        {
+            Dart.DartIns.DDart.transform.localPosition = new Vector3(0.0f, 0.07f, 1.3f);
+            Dart.DartIns.DDart.transform.localRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            Dart.DartIns.DDart.transform.localScale = new Vector3(4, 4, 4);
+            if (Dart.DartIns.DDart.transform.localPosition.x == 0.0)
+            {
+                GetDart = false;
+            }
+        }
     }
 }
