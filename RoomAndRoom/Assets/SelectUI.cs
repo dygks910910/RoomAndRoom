afd2f45ba692f70116c583b7c06bc56e15ab2e9b
HWANGJUNGWOO <ghkdwnddn@hanmail.net>
2017-12-19 20:39:57 +0900

    Ui씬 3D오브젝트 , 메뉴, 맵구성

---
+++ RoomAndRoom/Assets/SelectUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectUI : MonoBehaviour {
+    public SpriteRenderer sp;
+    public Color cr;
+    AudioSource audios;
+    public AudioClip buttonSD;
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        audios = GetComponent<AudioSource>();
+    }
+    public void SizeUp()
+    {
+        audios.PlayOneShot(buttonSD);
+        sp.color = new Color(0.0f,0.0f,0.0f,250.0f);
+        transform.localScale = new Vector3(0.4f, 0.4f, 1.0f);
+    }
+    public void SizeDown()
+    {
+        sp.color = cr;
+        transform.localScale = new Vector3(0.3f, 0.3f, 1.0f);
+    }
+
+}
---
+++ RoomAndRoom/Assets/UIpicture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIpicture : MonoBehaviour {
+    public GameObject Map;
+    public GameObject TT;
+    public static UIpicture Up;
+    public bool PictureCheck = false;
+    // Use this for initialization
+    void Awake()
+    {
+        UIpicture.Up = this;
+    }
+    public void ClickPicture()
+    {
+        Map.SetActive(true);
+    }
+    public void SizeUp()
+    {
+        TT.transform.localScale = new Vector3(0.3f, 0.3f, 1.0f);
+    }
+    public void SizeDown()
+    {
+        TT.transform.localScale = new Vector3(0.2f, 0.2f, 1.0f);
+    }
+    void Update()
+    {
+        if (PictureCheck == true)
+        {
+            Map.SetActive(false);
+            PictureCheck = false;
+        }
+    }
+}
