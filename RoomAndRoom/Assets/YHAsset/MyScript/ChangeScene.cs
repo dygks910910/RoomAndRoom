36ff4a4801159b7db6cb0283a90538539eea235c
HWANGJUNGWOO <ghkdwnddn@hanmail.net>
2017-12-20 19:38:00 +0900

    Intensity 값을 이용한 조명변경

--- RoomAndRoom/Assets/YHAsset/MyScript/ChangeScene.cs
+++ RoomAndRoom/Assets/YHAsset/MyScript/ChangeScene.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour {
-
+    public Light UIlight;
+    bool LightCheck=false;
     public void ChangeGameScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        LightCheck = true;
+        if (UIlight.intensity <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+    void Update()
+    {
+        if (LightCheck == true)
+        {
+            UIlight.intensity -=0.15f* Time.deltaTime;           
+            //LightCheck = false;
+        }
     }
 }
